dcb80c604d1a7617f2f9c1162557b6a94221b2f5
unknown <101371138+panzer-chan@users.noreply.github.com>
2023-12-13 21:09:19 -0500

    more handler changes
    also, no more sqlite file for you, github

--- src/handlers/event_handler.rs
+++ src/handlers/event_handler.rs
@@ -1,10 +1,11 @@
 pub mod event_handler {
     use serenity::async_trait;
+    use serenity::builder::CreateAllowedMentions;
     use serenity::client::EventHandler;
     use serenity::gateway::ActivityData;
     use serenity::model::channel::Message;
     use serenity::model::gateway::Ready;
-    use serenity::all::{Context, ResumedEvent, Guild, OnlineStatus, UnavailableGuild};
+    use serenity::all::{Context, ResumedEvent, Guild, OnlineStatus, UnavailableGuild, GuildChannel};
     use tracing::info;
 
     use crate::utilities::global_data::{DatabaseConnectionContainer, GuildSettingsContainer, GuildSettings};
@@ -19,60 +20,93 @@ pub mod event_handler {
         //
         // Event handlers are dispatched through a threadpool, and so multiple events can be dispatched
         // simultaneously.
-        async fn message(&self, _ctx: Context, _msg: Message) {
+        async fn message(&self, _ctx: Context, msg: Message) {
             // TODO: add advanced command handler + database connection
+
+            // ignore all bots, including the bot itself
+            if msg.author.bot {
+                return;
+            }
+
+            // trim the end to make it easier for mobile users
+            let content = msg.content.trim_end();
+
+            if content == "<@!1183487567094632638>" || content == "<@1183487567094632638>" {
+                let prefix = {
+                    let data = _ctx.data.read().await;
+                    let guild_settings = data.get::<GuildSettingsContainer>().unwrap();
+                    let pf = guild_settings.read().await;
+                    pf.get(&msg.guild_id.unwrap().get()).unwrap().prefix.clone()
+                };
+
+                let embed = serenity::builder::CreateEmbed::new()
+                .title("**Hello!**")
+                .description(format!("```To see the list of commands type {}help```", prefix));
+
+                let builder = serenity::builder::CreateMessage::new()
+                .add_embed(embed)
+                .allowed_mentions(CreateAllowedMentions::new().users(vec![msg.author.id]))
+                .reference_message(&msg);
+
+                msg.channel_id.send_message(&_ctx, builder).await.unwrap();
+            }
         }
 
-        async fn guild_create(&self, ctx: Context, guild: Guild, is_new: Option<bool>) {
-            // write into database and hashmap
-            if is_new == Some(true) {
-                info!("Joined guild: {}", guild.name);
-                info!("Guild ID: {}", guild.id);
-                info!("Guild Owner ID: {}", guild.owner_id);
-                info!("Guild Members: {}", guild.member_count);
-
-                let data = ctx.data.read().await;
-                let database = data.get::<DatabaseConnectionContainer>().unwrap().clone();
-                let (guild_id, owner_id) = {
-                    let guild_id = i64::from(guild.id);
-                    let owner_id = i64::from(guild.owner_id);
-
-                    (guild_id, owner_id)
-                };
-
-                sqlx::query!(
-                    "INSERT INTO guild_settings (
-                        guild_id,
-                        prefix,
-                        owner_id
-                    ) VALUES (?, ?, ?)",
-                    guild_id,
-                    "-",
-                    owner_id
-                ).execute(&database).await.unwrap();
-
-                let owner_id_u64 = owner_id as u64;
-                let guild_id_u64 = guild_id as u64;
-
-                let data_to_set = GuildSettings {
-                    prefix: "-".to_string(),
-                    owner_id: owner_id_u64,
-                    mute_type: "timeout".to_string(),
-                    mute_role: 0
-                };
-
-                {
-                    let mut guild_settings = data.get::<GuildSettingsContainer>().unwrap().write().await;
-                    guild_settings.insert(guild_id_u64, data_to_set);
-                }
-
-                info!("Guild settings set complete for guild {}", guild.name);
+        async fn thread_create(&self, ctx: Context, thread: GuildChannel) {
+            if let Err(err) = thread.id.join_thread(ctx.http).await {
+                let thread_id = thread.id;
+                info!("Failed to succesfully join thread (ID: {thread_id}): {err}")
             } else {
-                info!("Connected to guild: {}", guild.name);
-                info!("Guild ID: {}", guild.id);
-                info!("Guild Owner ID: {}", guild.owner_id);
-                info!("Guild Members: {}", guild.member_count);
+                let name = &thread.name;
+                let guild = &thread.guild(&ctx.cache).unwrap().name;
+                let id = thread.id.get();
+                info!("Joined new thread: {name} (Server: {guild}, ID: {id})")
             }
+        }
+
+        async fn guild_create(&self, ctx: Context, guild: Guild, _: Option<bool>) {
+            // write into database and hashmap
+            info!("Connected to guild: {}", guild.name);
+            info!("Guild ID: {}", guild.id);
+            info!("Guild Owner ID: {}", guild.owner_id);
+            info!("Guild Members: {}", guild.member_count);
+
+            let data = ctx.data.read().await;
+            let database = data.get::<DatabaseConnectionContainer>().unwrap().clone();
+            let (guild_id, owner_id) = {
+                let guild_id = i64::from(guild.id);
+                let owner_id = i64::from(guild.owner_id);
+
+                (guild_id, owner_id)
+            };
+
+            sqlx::query!(
+                "INSERT INTO guild_settings (
+                    guild_id,
+                    prefix,
+                    owner_id
+                ) VALUES (?, ?, ?) ON CONFLICT DO NOTHING",
+                guild_id,
+                "-",
+                owner_id
+            ).execute(&database).await.unwrap();
+
+            let owner_id_u64 = owner_id as u64;
+            let guild_id_u64 = guild_id as u64;
+
+            let data_to_set = GuildSettings {
+                prefix: "-".to_string(),
+                owner_id: owner_id_u64,
+                mute_type: "timeout".to_string(),
+                mute_role: 0
+            };
+
+            {
+                let mut guild_settings = data.get::<GuildSettingsContainer>().unwrap().write().await;
+                guild_settings.insert(guild_id_u64, data_to_set);
+            }
+
+            info!("Guild settings set complete for guild {}", guild.name);
         }
 
         async fn guild_delete(&self, ctx: Context, _: UnavailableGuild, g: Option<Guild>) {
